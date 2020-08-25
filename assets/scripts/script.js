6ed14372cfe4c8b5b041555f81219e64b868b8dd
David Garza <dgarza0413@gmail.com>
2020-08-24 22:11:14 -0500

    changed api response body to populate dom

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,12 +3,13 @@ var url = 'https://jsonplaceholder.typicode.com/posts'
 $.get(url, function (response) {
     console.log(response)
     var rowEl = $('<div>')
+    rowEl.addClass('row')
     $("#main").append(rowEl)
 
     for (var i = 0; i < response.length; i++) {
         var colEl = $('<div>')
         colEl.addClass('three column')
-        colEl.text(response.body)
+        colEl.text(response[i].body)
 
         rowEl.append(colEl)
     }
