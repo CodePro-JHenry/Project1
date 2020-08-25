d7bb69ea79fd328386faff7f87704858cb118cba
David Garza <dgarza0413@gmail.com>
2020-08-24 22:15:37 -0500

    reformatting the columns into three of twelve

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,11 +4,11 @@ $.get(url, function (response) {
     console.log(response)
     var rowEl = $('<div>')
     rowEl.addClass('row')
-    $("#main").append(rowEl)
+    $(".container").append(rowEl)
 
     for (var i = 0; i < response.length; i++) {
         var colEl = $('<div>')
-        colEl.addClass('three column')
+        colEl.addClass('three columns')
         colEl.text(response[i].body)
 
         rowEl.append(colEl)
