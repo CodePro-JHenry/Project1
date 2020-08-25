66907b69ff3d8bbb770c31b0664beea9f69a3d36
David Garza <dgarza0413@gmail.com>
2020-08-24 22:27:19 -0500

    reformat with jquery css to change the grid layout manually

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,10 +7,14 @@ $.get(url, function (response) {
     $(".container").append(rowEl)
 
     for (var i = 0; i < response.length; i++) {
+        // var rowEl = $('<div>')
         var colEl = $('<div>')
-        colEl.addClass('three columns')
+        colEl.addClass('one-half column')
+        colEl.css({ 'padding': '2%', 'marginLeft': '0%' })
         colEl.text(response[i].body)
-
+        // if (i % 3 === 0) {
+        //     rowEl.addClass('row')
+        // }
         rowEl.append(colEl)
     }
 })
